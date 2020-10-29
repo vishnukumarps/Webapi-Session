b5160ef0d91668c507c9e79735744790d6ac5d4e
vishnukumarps <vishnukumar29518@gmail.com>
2020-10-29 10:01:37 +0530

    Add project files.

---
+++ Webapi1/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webapi1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            var name = HttpContext.Session.GetString("name").ToString();
+            var age = HttpContext.Session.GetInt32("age");
+
+            return name + "   " + age;
+        }
+    }
+}
